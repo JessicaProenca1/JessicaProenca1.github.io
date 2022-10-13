814fb97c4fc6813e5e081f4ab263f84217845f30
Jessica <je.proenca@yahoo.com.br>
2022-10-13 18:00:51 -0300

    add funcao que faz uma animação do sobremim/projetos/curriculo

--- script.js
+++ script.js
@@ -21,3 +21,23 @@ function selecionaMenu() {
 selecionaMenu();
 
 //linha preta onde mostra qual parte do menu estou selecionando
+
+
+window.addEventListener('scroll', reveal);
+function reveal() {
+  var reveals = document.querySelectorAll('.reveal')
+  for (let i = 0; i < reveals.length; i += 1) {
+    var windowheight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 150;
+
+    if(revealTop < windowheight - revealPoint){
+      reveals[i].classList.add('active');
+    }
+    else {
+      reveals[i].classList.remove('active');
+    }
+
+  }
+}
+// função para animar o 'sobre mim/projetos/curriculo'
